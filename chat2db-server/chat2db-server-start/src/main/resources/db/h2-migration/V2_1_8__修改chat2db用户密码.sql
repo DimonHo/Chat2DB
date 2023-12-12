550459f581e0053a6f67485f20379c92c4723c62
敖癸 <hezhigang@tanzhou.cn>
2023-12-12 16:32:53 +0800

    chat2db如果不是默认密码, 不执行update

--- chat2db-server/chat2db-server-start/src/main/resources/db/h2-migration/V2_1_8__修改chat2db用户密码.sql
+++ chat2db-server/chat2db-server-start/src/main/resources/db/h2-migration/V2_1_8__修改chat2db用户密码.sql
@@ -1,2 +1,6 @@
 ## 密码为chat2db的加密字符串
-UPDATE `dbhub_user` SET password = '$2a$10$XRjuCZ1dRpY16axTHWnjV.ye1gXVCqN2m8TMCMy0izAsmxGzd9lR6', email = 'chat2db@chat2db.com' WHERE id = 2;
+UPDATE `dbhub_user`
+SET password = '$2a$10$XRjuCZ1dRpY16axTHWnjV.ye1gXVCqN2m8TMCMy0izAsmxGzd9lR6',
+    email    = 'chat2db@chat2db.com'
+WHERE id = 2
+  and password = 'chat2db';
